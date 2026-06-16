125cca0f66481cc4ce0e87cb94e2cd79cc6819f2
Zolan Givre <zolan@lizard-apps.com>
2026-06-16 11:02:24 +0200

    chore: auto generated file

--- dev/app/(payload)/admin/importMap.js
+++ dev/app/(payload)/admin/importMap.js
@@ -1,9 +1,10 @@
-import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-dry-run/client'
-import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-dry-run/rsc'
+import { BeforeDashboardClient as BeforeDashboardClient_909fa8a9ea6b80eed2f5235a24649347 } from 'payload-dry-run/client'
+import { BeforeDashboardServer as BeforeDashboardServer_b14b63438c9109c882f80c95b45a3833 } from 'payload-dry-run/rsc'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
+/** @type import('payload').ImportMap */
 export const importMap = {
-  'payload-dry-run/client#BeforeDashboardClient':
-    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
-  'payload-dry-run/rsc#BeforeDashboardServer':
-    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+  "payload-dry-run/client#BeforeDashboardClient": BeforeDashboardClient_909fa8a9ea6b80eed2f5235a24649347,
+  "payload-dry-run/rsc#BeforeDashboardServer": BeforeDashboardServer_b14b63438c9109c882f80c95b45a3833,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
